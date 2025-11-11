5954a7055b7c83856b3997a0daaa3e5d593d4d36
tainara <tainaraazevedo791@gmail.com>
2025-11-10 23:26:23 -0300

    ajustes

--- estoque/urls.py
+++ estoque/urls.py
@@ -2,11 +2,8 @@ from django.urls import path
 from . import views  # importa suas views locais
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('', views.index, name='index'),
-=======
->>>>>>> 035f39511a7fba4fcf823daf71445e3bff1c11b2
-    path('listar/', views.listar_estoque, name='listar_estoque'),
+ 
+    path('', views.index, name='index'),    path('listar/', views.listar_estoque, name='listar_estoque'),
     path('criar/', views.adcionar_estoque, name='adicionar_estoque'),
     path('editar/<int:item_id>/', views.editar_item, name='editar_item'),
     path('deletar/<int:item_id>/', views.deletar_item, name='deletar_item'),

035f39511a7fba4fcf823daf71445e3bff1c11b2
tainara <tainaraazevedo791@gmail.com>
2025-11-10 23:15:30 -0300

    Ajustes no admin e criação de regras de negócio (views)

--- estoque/urls.py
+++ estoque/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views  # importa suas views locais
 
 urlpatterns = [
-    path('', views.index, name='index'),
- 
+    path('listar/', views.listar_estoque, name='listar_estoque'),
+    path('criar/', views.adcionar_estoque, name='adicionar_estoque'),
+    path('editar/<int:item_id>/', views.editar_item, name='editar_item'),
+    path('deletar/<int:item_id>/', views.deletar_item, name='deletar_item'),
+    path('detalhar/<int:item_id>/', views.detalhar_item, name='detalhar_item'),
 ]

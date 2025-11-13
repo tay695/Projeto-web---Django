d4fb5df32ce4306a4dce5132ab57afdd6dd44c7a
Jessica Taina Rodrigues Silva <jessicataina2006@gmail.com>
2025-11-12 23:43:35 -0300

    model da entidade_beneficiada

---
+++ entidade_beneficiada/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EntidadeBeneficiadaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'entidade_beneficiada'
---
+++ entidade_beneficiada/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class EntidadeBeneficiada(models.Model):
+    PRIORIDADE_CHOICES = [
+        ('BAIXA', 'Baixa'),
+        ('MEDIA', 'Média'),
+        ('ALTA', 'Alta'),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255, verbose_name="ONG/Família")
+    responsavel = models.CharField(max_length=255)
+    endereco = models.CharField(max_length=255)
+    prioridade = models.CharField(max_length=5, choices=PRIORIDADE_CHOICES, default='BAIXA')
+    data_cadastro = models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        verbose_name = "Entidade Beneficiada"
+        verbose_name_plural = "Entidades Beneficiadas"

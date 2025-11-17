0951ba3771b9a6c4b571ba283a554c1edae3ceaa
tainara <tainaraazevedo791@gmail.com>
2025-11-17 10:57:38 -0300

    Adicionando app a settings

--- Doador/url.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views  # importa suas views locais
-
-urlpatterns = [
-    
-    path('', views.index, name='index'),
-]
--- projeto_web/settings.py
+++ projeto_web/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'estoque',
     'entidade_beneficiada'
+    'doador',
+    'doacao'
 ]
 
 MIDDLEWARE = [
